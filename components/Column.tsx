1f87a89e3a6e038feb70483c95213076c10c1e10
willegge <willeggedev@gmail.com>
2024-09-05 16:22:22 -0400

    chore: Add Column component for displaying a column in the board

---
+++ components/Column.tsx
@@ -0,0 +1,53 @@
+import { ColumnType } from "@/types/type";
+import { EmptyCardHolder } from "./EmptyCardHolder";
+import { CardList } from "./CardList";
+import { useEffect, useRef, useState } from "react";
+import { monitorForElements } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+
+export const Column = ({ column }: { column: ColumnType }) => {
+  const { id, name, cards } = column;
+  const ref = useRef<HTMLLIElement | null>(null);
+  const [highlight, setHighlight] = useState(false);
+
+  useEffect(() => {
+    const element = ref.current;
+
+    const monitorConfig = {
+      element,
+      onDrag({ location }) {
+        const target = location.current.dropTargets[0];
+
+        if (!target) {
+          return;
+        }
+
+        if (target.data.columnId === id) {
+          setHighlight(true);
+        } else {
+          setHighlight(false);
+        }
+      },
+      onDrop() {
+        setHighlight(false);
+      },
+    };
+
+    return monitorForElements(monitorConfig);
+  }, [id]);
+
+  return (
+    <li
+      className={`w-72 h-full shrink-0 ${
+        highlight ? "bg-blue-100" : "bg-gray-50"
+      } rounded-md`}
+      ref={ref}
+    >
+      <h2 className="p-4 text-slate-700">{name}</h2>
+      {cards.length > 0 ? (
+        <CardList cards={cards} />
+      ) : (
+        <EmptyCardHolder columnId={id} />
+      )}
+    </li>
+  );
+};
