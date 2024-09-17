18b1290d2e41d0fb89f9cbe40657b81d8624ebee
willegge <willeggedev@gmail.com>
2024-09-17 16:45:22 -0400

    feat: Add drag and drop functionality to EmptyCardHolder component

--- components/EmptyCardHolder.tsx
+++ components/EmptyCardHolder.tsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { dropTargetForElements } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
 
 const EmptyCardHolder = ({ columnId }: { columnId: string }) => {
+  const [aboutToDrop, setAboutToDrop] = useState(false);
+  const ref = useRef<HTMLLIElement | null>(null);
+
+  useEffect(() => {
+    const element = ref.current;
+    if (!element) {
+      return;
+    }
+
+    return dropTargetForElements({
+      element,
+      getData() {
+        return {
+          id: "placeholder",
+          position: 0,
+          columnId: columnId,
+        }
+      },
+      onDragEnter() {
+        setAboutToDrop(true);
+      },
+      onDragLeave() {
+        setAboutToDrop(false);
+      },
+      onDrop() {
+        setAboutToDrop(false);
+      },
+    });
+  }, []);
+
   return (
-    <ol className="p-4 flex flex-col gap-4" data-test-id={columnId}>
-      <li className={`relative p-2 hover:cursor-grab h-4`} />
+    <ol className="p-5 flex flex-col gap-4" data-test-id={columnId}>
+      <li
+        ref={ref}
+        className={`relative p-2 h-8 | ${aboutToDrop ? "bg-green-200" : ""}`}
+      />
     </ol>
   );
 };
