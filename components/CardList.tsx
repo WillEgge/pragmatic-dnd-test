1e995311f2bfac951019ffb88f60c5b78b11ecb2
willegge <willeggedev@gmail.com>
2024-10-16 12:23:52 -0400

    refactor: Update CardList component layout and styles

--- components/CardList.tsx
+++ components/CardList.tsx
@@ -3,7 +3,7 @@ import { Card } from "./Card";
 
 export const CardList = ({ cards }: { cards: CardType[] }) => {
   return (
-    <ol className="p-4 flex flex-col gap-4">
+    <ol className="space-y-4">
       {cards.map((card) => (
         <Card key={card.id} card={card} />
       ))}
