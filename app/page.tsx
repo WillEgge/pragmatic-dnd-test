7d0d354ef7633241dae497ad9ad9a8af95b698f3
willegge <willeggedev@gmail.com>
2024-10-16 12:23:22 -0400

    refactor: Add AddCardForm component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,15 @@
 
 import { BoardProvider } from "@/data/BoardProvider";
 import { Board } from "@/components/Board";
+import AddCardForm from "@/components/AddCardForm";
 
 export default function Home() {
   return (
     <BoardProvider>
-      <Board />
+      <div className="max-w-3xl mx-auto px-4 py-6 sm:px-6 lg:px-8">
+        <AddCardForm />
+        <Board />
+      </div>
     </BoardProvider>
   );
 }
