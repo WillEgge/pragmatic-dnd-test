42c03a8a8e9131eaa41f4c1c7387cc13e341181f
willegge <willeggedev@gmail.com>
2024-11-14 13:15:02 -0500

    Refactor: Update CardList component to include DropTarget for drag and drop functionality

--- components/CardList.tsx
+++ components/CardList.tsx
@@ -1,11 +1,25 @@
+import React from "react";
 import { CardType } from "@/types/type";
 import { Card } from "./Card";
+import { DropTarget } from "./DropTarget";
 
-export const CardList = ({ cards }: { cards: CardType[] }) => {
+export const CardList = ({
+  cards,
+  columnId,
+}: {
+  cards: CardType[];
+  columnId: string;
+}) => {
   return (
-    <ol className="space-y-4">
-      {cards.map((card) => (
-        <Card key={card.id} card={card} />
+    <ol className="space-y-2">
+      {" "}
+      {/* Reduced spacing from 'space-y-4' to 'space-y-2' */}
+      <DropTarget columnId={columnId} position={0} />
+      {cards.map((card, index) => (
+        <React.Fragment key={card.id}>
+          <Card card={card} />
+          <DropTarget columnId={columnId} position={index + 1} />
+        </React.Fragment>
       ))}
     </ol>
   );
